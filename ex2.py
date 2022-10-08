987cbf3724e6b8e1a5f042b907c63a3c949920a2
Maria Jordana <jordana-cavalcante@outlook.com>
2022-10-08 10:48:46 -0300

    src

---
+++ ex2.py
@@ -0,0 +1,14 @@
+# Questão 2
+estacionamento = [1, 5, 4, 3, 2, 6, 7]
+
+estacionamento.sort()
+
+print(estacionamento)
+
+#Questão 3
+
+# Sim, é possível. Para inverter uma String utilizando pilha,
+# inicialmente cada caracter da String seria inserido na pilha através da função push() a partir da posição 0 da String.
+# Após todos os caracteres serem inseridos, uma nova String receberia um a um os caracteres armazenados na pilha através da utilização da função pop().
+# Dessa forma, teríamos a palavra original invertida.
+
